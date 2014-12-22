9a382dd63f49f576c82b8e997219335960dd00d9
Olle Kvarnström <olle+git@iix.se>
2014-12-22 22:25:58 +0100

    Make other programs able to SIGINT lsh
    The program now checks if the SIGINT is received from this this or another
    program, and only turns SIGINTS from the same pid into newlines in stdin

--- src/signal.c
+++ src/signal.c
@@ -1,22 +1,26 @@
-#include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "signal.h"
 
 void
-send_newline(int signal)
+send_newline(int signal, siginfo_t *info, void *unused)
   {
     (void)signal;
-    write(STDIN_FILENO, "\n", 1);
+    (void)unused;
+    if (info->si_pid == 0) /* Send by this program */
+      write(STDIN_FILENO, "\n", 1);
+    else
+      exit(0);
   }
 
 int
 init_signals(void)
   {
     struct sigaction action;
-    action.sa_handler = send_newline;
-    action.sa_flags = 0;
+    action.sa_sigaction = send_newline;
+    action.sa_flags = SA_SIGINFO;
     sigemptyset(&action.sa_mask);
 
     if (sigaction(SIGINT, &action, NULL) != 0)
--- src/signal.h
+++ src/signal.h
@@ -1,8 +1,10 @@
 #ifndef LSH_SIGNAL_H
 #define LSH_SIGNAL_H
 
+#include <signal.h>
+
 void
-send_newline(int signal);
+send_newline(int signal, siginfo_t *info, void *unused);
 
 int
 init_signals(void);
